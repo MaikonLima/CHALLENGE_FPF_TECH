27e8a7b2a254cc995a64b8e918264e9e12fa50c3
MaikonLima <andersonmaikon93@gmail.com>
2021-01-10 01:56:48 -0400

    delete_ok

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -2,17 +2,28 @@ import React, { useEffect, useState } from "react";
 import './styles.css';
 
 
-import { Card } from 'react-bootstrap';
+import { Card, Button} from 'react-bootstrap';
 // import api from "../../services/api";
 
 
 export default function Home() {
     return (
         <div>
-            <header>Gestão de Projetos
-            </header>
+            <header>Gestão de Projetos</header>
+            <br></br>
+            <br></br>
             <div className="container">
-                <h1>Menu</h1>
+                <Card style={{ width: '18rem' }}>
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     );
